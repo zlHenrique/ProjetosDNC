478038cd95fae71991cadc46f75d485f0f0b35d9
zlHenrique <heckv.camelo@gmail.com>
2023-09-06 18:10:14 -0300

    Adicionado interação nos cards

--- Projeto-CCPX/script.js
+++ Projeto-CCPX/script.js
@@ -1,28 +1,4 @@
-const dia = document.getElementById('dia')
-const hora = document.getElementById('hora')
-const minuto = document.getElementById('minuto')
-const segundo = document.getElementById('segundo')
-
-const lancamento = "1 dec 2023"
-
-function countDown(){
-    const dataLancamento = new Date(lancamento)
-    const dataAtual = new Date()
-    const totalSegundos = (dataLancamento - dataAtual)/1000;
-    const finalDias = Math.floor(totalSegundos / 60 / 60 / 24)
-    const finalHoras = Math.floor(totalSegundos / 60 / 60) % 24
-    const finalMinutos = Math.floor(totalSegundos / 60) % 60
-    const finalSegundos = Math.floor(totalSegundos) % 60
-
-    dia.innerHTML = formatoTempo(`${finalDias}D`)
-    hora.innerHTML = formatoTempo(`${finalHoras}H`)
-    minuto.innerHTML = formatoTempo(`${finalMinutos}M`)
-    segundo.innerHTML = formatoTempo(`${finalSegundos}S`)
+function highlightCard(selector) {
+    let element = document.querySelector(selector);
+    element.classList.toggle('card-highlight'); 
 }
-
-function formatoTempo(tempo) {
-    return tempo < 10? `0${tempo}` : tempo;
-}
-
-countDown()
-setInterval(countDown, 1000)

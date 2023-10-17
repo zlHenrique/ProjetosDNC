b479066e27a60dd82559d1bcdf617b5fe35f81c1
zlHenrique <heckv.camelo@gmail.com>
2023-10-17 15:04:49 -0300

    Ajustado site para celular.

--- Projeto-CCPX/script.js
+++ Projeto-CCPX/script.js
@@ -1,4 +1,46 @@
+let cardQuinta = document.getElementById('qui')
+let cardSexta = document.getElementById('sex')
+let cardSabado = document.getElementById('sab')
+let cardDomingo = document.getElementById('dom')
+let listaCards = [cardQuinta, cardSexta, cardSabado, cardDomingo]
+
 function highlightCard(selector) {
     let element = document.querySelector(selector);
-    element.classList.toggle('card-highlight'); 
+    element.classList.toggle('card-highlight');
 }
+
+function addKeyboardEventListeners () {
+    document.addEventListener('keydown', (event) => {
+        var code = event.code
+
+        if (code === 'Digit1') {
+            cardQuinta.classList.toggle('card-highlight')
+            cardSexta.classList.remove('card-highlight')
+            cardSabado.classList.remove('card-highlight')
+            cardDomingo.classList.remove('card-highlight')
+        }
+
+        if (code === 'Digit2') {
+            cardQuinta.classList.remove('card-highlight')
+            cardSexta.classList.toggle('card-highlight')
+            cardSabado.classList.remove('card-highlight')
+            cardDomingo.classList.remove('card-highlight')
+        }
+
+        if (code === 'Digit3') {
+            cardQuinta.classList.remove('card-highlight')
+            cardSexta.classList.remove('card-highlight')
+            cardSabado.classList.toggle('card-highlight')
+            cardDomingo.classList.remove('card-highlight')
+        }
+
+        if (code === 'Digit4') {
+            cardQuinta.classList.remove('card-highlight')
+            cardSexta.classList.remove('card-highlight')
+            cardSabado.classList.remove('card-highlight')
+            cardDomingo.classList.toggle('card-highlight')
+        }
+    })
+}
+
+addKeyboardEventListeners()
---
+++ Projeto-CCPX/teste.js
@@ -0,0 +1,12 @@
+let lista = [3, 5, 10, 20]
+
+
+function findAverage(array) {
+    const result = 0
+    let resultado
+    for (let i = 0; i <= array.length; i++) {
+        console.log(i)
+    }
+  }
+
+  findAverage(lista)
